39c74a6a38dd77734b461b758d54b83907989eca
Cameron Williams <cameronrw.nz@gmail.com>
2018-09-01 15:45:46 +0700

    Basic
    Basic Web API Get call to Mongo DB

---
+++ ASPWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using ASPWebAPI.Models;
+using ASPWebAPI.Services;
+
+namespace ASPWebAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private DataAccess _db;
+
+        Product[] products = new Product[]
+        {
+            new Product { ProductId = 1, ProductName = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { ProductId = 2, ProductName = "Yo-yo", Category = "Toys", Price = 3 },
+            new Product { ProductId = 3, ProductName = "Hammer", Category = "Hardware", Price = 16 }
+        };
+
+        public ProductsController()
+        {
+            _db = new DataAccess();
+        }
+
+        // GET: api/Products
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _db.GetProducts();
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ ASPWebAPI/Services/DataAccess.cs
@@ -0,0 +1,24 @@
+﻿using ASPWebAPI.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace ASPWebAPI.Services
+{
+    public class DataAccess
+    {
+        private MongoClient _client;
+        private IMongoDatabase _db;
+
+        public DataAccess()
+        {
+            _client = new MongoClient("mongodb://localhost:27017");
+            _db = _client.GetDatabase("EmployeeDB");
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _db.GetCollection<Product>("Products").Find(new BsonDocument()).ToEnumerable();
+        }
+    }
+}
